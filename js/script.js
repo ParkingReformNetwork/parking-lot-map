87ad4722bc447464579e6f93251d459674fef313
Eric Arellano <14852634+Eric-Arellano@users.noreply.github.com>
2023-04-04 13:48:48 -0600

    Refactor to not have global side effects & state in script.js (#12)
    To fix
    https://github.com/ParkingReformNetwork/parking-lot-map/issues/10, I
    want to add unit tests to test that we can handle several edge cases.
    But our unit tests fail when importing `script.js` because at import
    time, we do a bunch of side effects like using `L` (leaflet) and using
    Jquery to modify the document.
    
    Instead, to be able to import `script.js`, we can't have any "side
    effects". All we should do when importing is define our functions.
    
    Specifically, we have a new function `setUpSite()`, which will initiate
    all the side effects when called. Our HTML then does this:
    
    ```html
        <script type="text/javascript" src="js/script.js"></script>
        <script>
          setUpSite();
        </script>
    ```
    
    As a result, we can now add a simple unit test to confirm the import
    works!
    
    This is also a good refactor. It removes all global state from
    `script.js`. Functions can only access what they specifically are passed
    in as arguments. For example, it's now obvious where exactly we are
    modifying the `map`.

--- js/script.js
+++ js/script.js
@@ -1,64 +1,60 @@
-const googleMapsSatellite = L.tileLayer(
-  "https://mt1.google.com/vt/lyrs=s&x={x}&y={y}&z={z}",
-  {
-    attribution: "Google Maps",
-    maxZoom: 20, // max value is 24 but I set it to 20, because the Stamen Toner style has value of 20 as max value.
-  }
-);
+const setUpAbout = () => {
+  $(".banner-about").click(() => {
+    if ($(".about-text-popup").css("display") === "none") {
+      $(".about-text-popup").css("display", "block");
+    } else {
+      $(".about-text-popup").css("display", "none");
+    }
+  });
 
-const stamenToner = L.tileLayer(
-  "https://stamen-tiles-{s}.a.ssl.fastly.net/toner-lite/{z}/{x}/{y}{r}.{ext}",
-  {
-    attribution:
-      'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-    subdomains: "abcd",
-    minZoom: 0,
-    maxZoom: 20,
-    ext: "png",
-  }
-);
-
-$(".banner-about").click(() => {
-  if ($(".about-text-popup").css("display") === "none") {
-    $(".about-text-popup").css("display", "block");
-  } else {
-    $(".about-text-popup").css("display", "none");
-  }
-});
-
-$(".about-close").click(() => {
-  if ($(".about-text-popup").css("display") === "block") {
-    $(".about-text-popup").css("display", "none");
-  }
-});
-
-const map = L.map("map", {
-  zoomControl: false,
-  layers: [stamenToner],
-});
-
-const attribution = map.attributionControl;
-attribution.setPrefix(
-  'created by <a style="padding: 0 3px 0 3px; color:#fafafa; background-color: #21ccb9;" target="_blank" href=http://www.geocadder.bg/en/>GEOCADDER</a>'
-);
-
-const baseLayers = {
-  Light: stamenToner,
-  "Google Maps": googleMapsSatellite,
+  $(".about-close").click(() => {
+    if ($(".about-text-popup").css("display") === "block") {
+      $(".about-text-popup").css("display", "none");
+    }
+  });
 };
 
-L.control.layers(baseLayers).addTo(map);
+const defineBaseLayers = () => ({
+  Light: L.tileLayer(
+    "https://stamen-tiles-{s}.a.ssl.fastly.net/toner-lite/{z}/{x}/{y}{r}.{ext}",
+    {
+      attribution:
+        'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+      subdomains: "abcd",
+      minZoom: 0,
+      maxZoom: 20,
+      ext: "png",
+    }
+  ),
+  "Google Maps": L.tileLayer(
+    "https://mt1.google.com/vt/lyrs=s&x={x}&y={y}&z={z}",
+    {
+      attribution: "Google Maps",
+      // Max value is 24 but I set it to 20 because the Stamen Toner style has value of 20 as max value.
+      maxZoom: 20,
+    }
+  ),
+});
 
-// We need to set `pane`, which comes from Leaflet.
-pane = map.createPane("fixed", document.getElementById("map"));
+const createMap = () => {
+  const baseLayers = defineBaseLayers();
+  const map = L.map("map", {
+    zoomControl: false,
+    layers: [baseLayers.Light],
+  });
+  map.attributionControl.setPrefix(
+    'created by <a style="padding: 0 3px 0 3px; color:#fafafa; background-color: #21ccb9;" target="_blank" href=http://www.geocadder.bg/en/>GEOCADDER</a>'
+  );
+  L.control.layers(baseLayers).addTo(map);
+  // We need to set `pane`, which comes from Leaflet.
+  pane = map.createPane("fixed", document.getElementById("map"));
 
-let currentTier = $("#tier-dropdown option:selected").text();
-
-// added initial zoom
-const zoomHome = L.Control.zoomHome();
-zoomHome.setHomeCoordinates([39.440556, -98.697222]);
-zoomHome.setHomeZoom(4);
-zoomHome.addTo(map);
+  const zoomHome = L.Control.zoomHome();
+  zoomHome.setHomeCoordinates([39.440556, -98.697222]);
+  zoomHome.setHomeZoom(4);
+  zoomHome.addTo(map);
+  return map;
+};
 
 const citiesPolygonsStyle = {
   fillColor: "##c84041",
@@ -73,115 +69,124 @@ const parkingLotsStyle = {
   fillOpacity: 0.6,
 };
 
-// adding URL tags
-let locationTag = "";
+const determineAndSetUpLocationTag = (url) => {
+  let result = "";
 
-const urlAddress = window.location.href;
-if (urlAddress.indexOf("#parking-reform-map") > -1) {
-  const [, splitAddressFragment] = urlAddress.split("#");
-  // If the tag is "parking-reform-map=....."
-  if (splitAddressFragment.indexOf("parking-reform-map") > -1) {
-    $("#toggle-projects-by-tag").css("display", "none");
-    const [, locationTagFromUrl] = splitAddressFragment.split("=");
-    locationTag = locationTagFromUrl;
+  if (url.indexOf("#parking-reform-map") > -1) {
+    const [, splitAddressFragment] = url.split("#");
+    // If the tag is "parking-reform-map=....."
+    if (splitAddressFragment.indexOf("parking-reform-map") > -1) {
+      $("#toggle-projects-by-tag").css("display", "none");
+      const [, locationTagFromUrl] = splitAddressFragment.split("=");
+      result = locationTagFromUrl;
+    }
   }
-}
 
-const [firstTag, ...remainingTags] = locationTag.split("%20");
-if (remainingTags.length > 0) {
-  locationTag = `${firstTag} ${remainingTags.join(" ")}`;
-}
-// end adding URL tags
+  const [firstTag, ...remainingTags] = result.split("%20");
+  if (remainingTags.length > 0) {
+    result = `${firstTag} ${remainingTags.join(" ")}`;
+  }
+  return result;
+};
 
-// 1. start Cities Polygons layer
-$.getJSON("data/cities-polygons.geojson", (data) => {
-  const citiesArray = [];
-  const citiesPolygons = L.geoJson(data, {
-    style() {
-      return citiesPolygonsStyle;
-    },
-    onEachFeature(feature, layer) {
-      $("#tier-dropdown").change(() => {
-        currentTier = $("#tier-dropdown option:selected").text();
+const setUpCitiesLayer = (map, locationTag) => {
+  let currentTier = $("#tier-dropdown option:selected").text();
+  $.getJSON("data/cities-polygons.geojson", (data) => {
+    const citiesArray = [];
+    const citiesPolygons = L.geoJson(data, {
+      style() {
+        return citiesPolygonsStyle;
+      },
+      onEachFeature(feature, layer) {
+        $("#tier-dropdown").change(() => {
+          currentTier = $("#tier-dropdown option:selected").text();
 
-        if (currentTier === feature.properties.Name) {
-          map.fitBounds(layer.getBounds());
-          const popupContent = generatePopupContent(feature);
-          const popup = L.popup({
-            pane: "fixed",
-            className: "popup-fixed",
-            autoPan: false,
-          }).setContent(popupContent);
-          layer.bindPopup(popup).openPopup();
-        }
-      });
-
-      // checking for the URL tag
-      const [cityName] = feature.properties.Name.split(", ");
-      if (cityName.toLowerCase() === locationTag.toLowerCase()) {
-        const popupContent = generatePopupContent(feature);
-
-        const popup = L.popup({
-          pane: "fixed",
-          className: "popup-fixed",
-          autoPan: false,
-        }).setContent(popupContent);
-
-        map.fitBounds(layer.getBounds());
-        layer.once("add", () => {
-          layer.bindPopup(popup).openPopup();
+          if (currentTier === feature.properties.Name) {
+            map.fitBounds(layer.getBounds());
+            const popupContent = generatePopupContent(map, feature);
+            const popup = L.popup({
+              pane: "fixed",
+              className: "popup-fixed",
+              autoPan: false,
+            }).setContent(popupContent);
+            layer.bindPopup(popup).openPopup();
+          }
         });
-      }
-      // end checking for the URL tag
 
-      citiesArray.push(feature.properties.Name);
+        // checking for the URL tag
+        const [cityName] = feature.properties.Name.split(", ");
+        if (cityName.toLowerCase() === locationTag.toLowerCase()) {
+          const popupContent = generatePopupContent(map, feature);
 
-      layer.on({
-        click() {
-          const popupContent = generatePopupContent(feature);
           const popup = L.popup({
             pane: "fixed",
             className: "popup-fixed",
             autoPan: false,
           }).setContent(popupContent);
-          layer.bindPopup(popup).openPopup();
-          layer.bindPopup(popupContent).openPopup();
-        },
-      });
-    },
-  }).addTo(map);
 
-  citiesArray.sort((a, b) => a.localeCompare(b));
-  citiesArray.forEach((city) =>
-    $("#city-choice").append(
-      $("<option></option>").attr("value", city).text(city)
-    )
-  );
+          map.fitBounds(layer.getBounds());
+          layer.once("add", () => {
+            layer.bindPopup(popup).openPopup();
+          });
+        }
+        // end checking for the URL tag
 
-  if (urlAddress.indexOf("#parking-reform-map") === -1) {
-    map.fitBounds(citiesPolygons.getBounds());
-  }
-}).then(() => {
-  // Select default city
-  $("#city-choice").val("Columbus, OH").change();
-});
-// 1. end Cities Polygons layer
+        citiesArray.push(feature.properties.Name);
 
-// 2. start Parking Lots layer
-$.getJSON("data/parking-lots.geojson", (data) => {
-  L.geoJson(data, {
-    style() {
-      return parkingLotsStyle;
-    },
-  }).addTo(map);
-});
-// 2. end Parking Lots layer
+        layer.on({
+          click() {
+            const popupContent = generatePopupContent(map, feature);
+            const popup = L.popup({
+              pane: "fixed",
+              className: "popup-fixed",
+              autoPan: false,
+            }).setContent(popupContent);
+            layer.bindPopup(popup).openPopup();
+            layer.bindPopup(popupContent).openPopup();
+          },
+        });
+      },
+    }).addTo(map);
 
-function generatePopupContent(feature) {
+    citiesArray.sort((a, b) => a.localeCompare(b));
+    citiesArray.forEach((city) =>
+      $("#city-choice").append(
+        $("<option></option>").attr("value", city).text(city)
+      )
+    );
+
+    if (window.location.href.indexOf("#parking-reform-map") === -1) {
+      map.fitBounds(citiesPolygons.getBounds());
+    }
+  }).then(() => {
+    // Select default city
+    $("#city-choice").val("Columbus, OH").change();
+  });
+};
+
+const setUpParkingLotsLayer = (map) => {
+  $.getJSON("data/parking-lots.geojson", (data) => {
+    L.geoJson(data, {
+      style() {
+        return parkingLotsStyle;
+      },
+    }).addTo(map);
+  });
+};
+
+const setUpSite = () => {
+  setUpAbout();
+  const map = createMap();
+  const locationTag = determineAndSetUpLocationTag(window.location.href);
+  setUpCitiesLayer(map, locationTag);
+  setUpParkingLotsLayer(map);
+};
+
+function generatePopupContent(map, feature) {
   let popupContent = `<div class='title'>${feature.properties.Name}</div><div class='url-copy-button'><a href='#'><img src='icons/share-url-button.png'></a></div><hr>`;
 
   // copy the URL for the current city
-  let currentLocationUrl = urlAddress;
+  let currentLocationUrl = window.location.href;
   const [cityName] = feature.properties.Name.toLowerCase().split(", ");
   if (currentLocationUrl.indexOf("#parking-reform-map=") > -1) {
     const urlTagArray = currentLocationUrl.split("#parking-reform-map=");
@@ -223,3 +228,7 @@ function generatePopupContent(feature) {
   }
   return popupContent;
 }
+
+module.exports = {
+  setUpSite,
+};
