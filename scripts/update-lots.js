9a21525fbac3b65c14626b307c8b4e738bc1b989
Eric Arellano <14852634+Eric-Arellano@users.noreply.github.com>
2023-04-05 20:04:42 -0600

    Add `npm run update-lots` and update Detroit & Anchorage, (#27)
    Data updates:
    
    1. Anchorage score card
    2. Anchorage city boundary
    3. Anchorage parking lots
    4. Detroit score card
    5. Detroit parking lots
    
    This adds the new command `npm run update-lots -- 'My City, AZ'. It
    expects you to have the file `parking-lots-update.geojson` in the root
    of the repository with data for a single city's parking lots. Then it
    will update `data/parking-lots.geojson` for you.
    
    <img width="1064" alt="Captura de pantalla 2023-04-05 a la(s) 7 59 14 p
    m"
    src="https://user-images.githubusercontent.com/14852634/230253362-f7df99da-5fe3-43f1-b7a1-d965069422d6.png">
    
    <img width="1118" alt="Captura de pantalla 2023-04-05 a la(s) 7 59 32 p
    m"
    src="https://user-images.githubusercontent.com/14852634/230253397-751105ba-a980-47dd-8f54-2c4f94ba3564.png">

---
+++ scripts/update-lots.js
@@ -0,0 +1,58 @@
+const fs = require("fs").promises;
+
+const updateGeoJSON = async (cityName) => {
+  try {
+    const jsonStringNewData = await fs.readFile(
+      "parking-lots-update.geojson",
+      "utf8"
+    );
+    const newData = JSON.parse(jsonStringNewData);
+
+    if (!Array.isArray(newData.features) || newData.features.length !== 1) {
+      console.error(
+        "The script expects exactly one entry in `features` because you can only update one city at a time."
+      );
+      return;
+    }
+
+    const newCoordinates = newData.features[0].geometry.coordinates;
+
+    const jsonStringOriginalData = await fs.readFile(
+      "data/parking-lots.geojson",
+      "utf8"
+    );
+    const originalData = JSON.parse(jsonStringOriginalData);
+
+    const cityOriginalData = originalData.features.find(
+      (feature) => feature.properties.Name === cityName
+    );
+
+    if (!cityOriginalData) {
+      console.error(
+        "The program only works on cities currently in the data set."
+      );
+      return;
+    }
+
+    cityOriginalData.geometry.coordinates = newCoordinates;
+
+    await fs.writeFile(
+      "data/parking-lots.geojson",
+      JSON.stringify(originalData, null, 2)
+    );
+    console.log(
+      "File updated successfully! Now, run `npm run fmt`. Then, `npm start` and see if the site is what you expect"
+    );
+  } catch (err) {
+    console.error("Error:", err);
+  }
+};
+
+const cityName = process.argv[2];
+if (!cityName) {
+  console.error(
+    "Please provide a city/state name as an argument, e.g. `npm run update-lots -- 'Columbus, OH'`"
+  );
+  process.exit(1);
+}
+updateGeoJSON(cityName);
