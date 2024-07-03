5e7a3488fa229fa5d0945ef9ff7c96245e207bc8
Eric Arellano <ericarellano@me.com>
2024-07-03 07:57:17 -0400

    Use early returns

--- src/js/about.ts
+++ src/js/about.ts
@@ -4,38 +4,39 @@
  * Set up event listeners to open and close the about popup.
  */
 const setUpAbout = () => {
-  const aboutElement: HTMLElement | null =
-    document.querySelector(".about-text-popup");
+  const aboutElement = document.querySelector(".about-text-popup");
   const infoButton = document.querySelector(".header-about-icon-container");
-  if (infoButton && aboutElement) {
-    infoButton.addEventListener("click", () => {
-      aboutElement.style.display =
-        aboutElement.style.display !== "block" ? "block" : "none";
-    });
+  if (
+    !(aboutElement instanceof HTMLElement) ||
+    !(infoButton instanceof HTMLElement)
+  )
+    return;
 
-    // closes window on clicks outside the info popup
-    window.addEventListener("click", (event: MouseEvent) => {
-      const clickTarget = event?.target as Element;
-      if (
-        !infoButton.contains(clickTarget) &&
-        aboutElement.style.display === "block" &&
-        !aboutElement.contains(clickTarget)
-      ) {
-        aboutElement.style.display = "none";
-        infoButton.classList.toggle("active");
-      }
-    });
+  infoButton.addEventListener("click", () => {
+    aboutElement.style.display =
+      aboutElement.style.display !== "block" ? "block" : "none";
+  });
 
+  // closes window on clicks outside the info popup
+  window.addEventListener("click", (event) => {
+    if (
+      event.target instanceof Element &&
+      !infoButton.contains(event.target) &&
+      aboutElement.style.display === "block" &&
+      !aboutElement.contains(event.target)
+    ) {
+      aboutElement.style.display = "none";
+      infoButton.classList.toggle("active");
+    }
+  });
+
+  const aboutClose = document.querySelector(".about-close");
+  if (!(aboutClose instanceof HTMLElement)) return;
+  aboutClose.addEventListener("click", () => {
     // Note that the close element will only render when the about text popup is rendered.
     // So, it only ever makes sense for a click to close.
-    const aboutClose: HTMLAnchorElement | null =
-      document.querySelector(".about-close");
-    if (aboutClose) {
-      aboutClose.addEventListener("click", () => {
-        aboutElement.style.display = "none";
-      });
-    }
-  }
+    aboutElement.style.display = "none";
+  });
 };
 
 export default setUpAbout;
--- src/js/share.ts
+++ src/js/share.ts
@@ -12,41 +12,33 @@ const copyToClipboard = async (value: string): Promise<void> => {
 };
 
 const switchIcons = (shareIcon: HTMLAnchorElement): void => {
-  const linkIcon: HTMLAnchorElement | null = shareIcon.querySelector(
-    "svg.share-link-icon"
-  );
-  const checkIcon: HTMLAnchorElement | null = shareIcon.querySelector(
-    "svg.share-check-icon"
-  );
-  if (linkIcon && checkIcon) {
-    linkIcon.style.display = "none";
-    checkIcon.style.display = "inline-block";
-    setTimeout(() => {
-      linkIcon.style.display = "inline-block";
-      checkIcon.style.display = "none";
-    }, 1000);
-  }
+  const linkIcon = shareIcon.querySelector("svg.share-link-icon");
+  const checkIcon = shareIcon.querySelector("svg.share-check-icon");
+  if (!(linkIcon instanceof SVGElement) || !(checkIcon instanceof SVGElement))
+    return;
+
+  linkIcon.style.display = "none";
+  checkIcon.style.display = "inline-block";
+  setTimeout(() => {
+    linkIcon.style.display = "inline-block";
+    checkIcon.style.display = "none";
+  }, 1000);
 };
 
 const setUpShareUrlClickListener = (cityId: CityId): void => {
   // We put the event listener on `map` because it is never erased, unlike the copy button
   // being recreated every time the score card changes. This is called "event delegation".
-  const mapElement: HTMLElement | null = document.querySelector("#map");
-  if (mapElement) {
-    mapElement.addEventListener("click", async (event: MouseEvent | null) => {
-      const copyButton = event?.target as Element;
-      if (copyButton) {
-        const targetElement: HTMLAnchorElement | null = copyButton.closest(
-          "div.url-copy-button > a"
-        );
-        if (targetElement) {
-          const shareUrl = determineShareUrl(window.location.href, cityId);
-          await copyToClipboard(shareUrl);
-          switchIcons(targetElement);
-        }
-      }
-    });
-  }
+  const mapElement = document.querySelector("#map");
+  if (!(mapElement instanceof Element)) return;
+  mapElement.addEventListener("click", async (event) => {
+    const copyButton = event.target;
+    if (!(copyButton instanceof Element)) return;
+    const targetElement = copyButton.closest("div.url-copy-button > a");
+    if (!(targetElement instanceof HTMLAnchorElement)) return;
+    const shareUrl = determineShareUrl(window.location.href, cityId);
+    await copyToClipboard(shareUrl);
+    switchIcons(targetElement);
+  });
 };
 
 export default setUpShareUrlClickListener;
