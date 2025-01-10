19e7917b355334bba7aad4ba5006042dc43f15e4
Eric Arellano <ericarellano@me.com>
2025-01-10 18:42:12 -0500

    Simplify approach to JSON type declarations

--- packages/primary/src/js/declarations.td.ts
+++
@@ -1,9 +0,0 @@
-declare module "*/data/city-boundaries.geojson" {
-  const value: import("@prn-parking-lots/shared/src/js/types").CityBoundaries;
-  export default value;
-}
-
-declare module "*/data/city-stats.json" {
-  const value: import("@prn-parking-lots/shared/src/js/types").CityStatsCollection;
-  export default value;
-}
