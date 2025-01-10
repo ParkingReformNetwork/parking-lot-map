ba9a1e2be0c9e2f9b9ac086482aee51f72043f1c
Eric Arellano <14852634+Eric-Arellano@users.noreply.github.com>
2025-01-10 18:44:58 -0500

    Simplify approach to JSON type declarations (#295)
    The declarations file wasn't working well when adding `packages/ct`.

--- packages/primary/src/js/data.ts
+++ packages/primary/src/js/data.ts
@@ -1,9 +1,17 @@
-/* eslint-disable import/no-relative-packages */
+/* eslint-disable import/first, import/newline-after-import */
 
-import type { ParkingLotGeoJSONModules } from "@prn-parking-lots/shared/src/js/types";
+import type {
+  ParkingLotGeoJSONModules,
+  CityStatsCollection,
+  CityBoundaries,
+} from "@prn-parking-lots/shared/src/js/types";
 
-import CITY_STATS_DATA from "../../data/city-stats.json" with { type: "json" };
-import CITY_BOUNDARIES_GEOJSON from "../../data/city-boundaries.geojson" with { type: "json" };
+import UNTYPED_CITY_STATS_DATA from "../../data/city-stats.json" with { type: "json" };
+const CITY_STATS_DATA: CityStatsCollection = UNTYPED_CITY_STATS_DATA;
+
+// @ts-expect-error TypeScript doesn't understand GeoJSON.
+import UNTYPED_CITY_BOUNDARIES_GEOJSON from "../../data/city-boundaries.geojson" with { type: "json" };
+const CITY_BOUNDARIES_GEOJSON: CityBoundaries = UNTYPED_CITY_BOUNDARIES_GEOJSON;
 
 const PARKING_LOT_GEOJSON_MODULES = import(
   // @ts-expect-error Dynamic import with glob pattern
