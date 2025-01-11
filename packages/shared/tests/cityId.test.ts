2bb0f19987b330866e2132b9cf25ad275aef3770
Eric Arellano <ericarellano@me.com>
2025-01-10 21:16:17 -0500

    Simplify anchor for sharing links

--- packages/shared/tests/cityId.test.ts
+++ packages/shared/tests/cityId.test.ts
@@ -10,37 +10,28 @@ test.describe("extractCityIdFromUrl()", () => {
     expect(extractCityIdFromUrl("")).toBeNull();
     expect(extractCityIdFromUrl("https://parking.org")).toBeNull();
     expect(extractCityIdFromUrl("https://parking.org#shoup")).toBeNull();
+    expect(extractCityIdFromUrl("https://parking.org#city")).toBeNull();
     expect(
       extractCityIdFromUrl("https://parking.org#parking-reform-map"),
     ).toBeNull();
   });
 
   test("extracts the city id", () => {
+    expect(extractCityIdFromUrl("https://parking.org#city=my-city")).toEqual(
+      "my-city",
+    );
     expect(
-      extractCityIdFromUrl("https://parking.org#parking-reform-map=city"),
-    ).toEqual("city");
-    expect(
-      extractCityIdFromUrl("https://parking.org#parking-reform-map=CITY"),
-    ).toEqual("city");
-    expect(
-      extractCityIdFromUrl(
-        "https://parking.org#parking-reform-map=city-of-shoup",
-      ),
-    ).toEqual("city-of-shoup");
-    expect(
-      extractCityIdFromUrl(
-        "https://parking.org#parking-reform-map=CITY-OF-SHOUP",
-      ),
-    ).toEqual("city-of-shoup");
-    expect(
-      extractCityIdFromUrl("https://parking.org#parking-reform-map=st.-louis"),
-    ).toEqual("st-louis");
+      extractCityIdFromUrl("https://parking.org#parking-reform-map=MY-CITY"),
+    ).toEqual("my-city");
+    expect(extractCityIdFromUrl("https://parking.org#city=st.-louis")).toEqual(
+      "st-louis",
+    );
   });
 });
 
 test("parseCityIdFromJson() extracts the city", () => {
-  expect(parseCityIdFromJson("City, AZ")).toEqual("city-az");
-  expect(parseCityIdFromJson("CITY, AZ")).toEqual("city-az");
+  expect(parseCityIdFromJson("Tempe, AZ")).toEqual("tempe-az");
+  expect(parseCityIdFromJson("TEMPE, AZ")).toEqual("tempe-az");
   expect(parseCityIdFromJson("Saint Shoup Village, AZ")).toEqual(
     "saint-shoup-village-az",
   );
@@ -55,25 +46,22 @@ test("parseCityIdFromJson() extracts the city", () => {
 
 test.describe("determineShareUrl()", () => {
   test("adds #parking-reform-map= if not yet present", () => {
-    expect(determineShareUrl("https://parking.org", "city-az")).toEqual(
-      "https://parking.org#parking-reform-map=city-az",
+    expect(determineShareUrl("https://parking.org", "tempe-az")).toEqual(
+      "https://parking.org#city=tempe-az",
     );
     expect(
       determineShareUrl("https://parking.org", "saint-shoup-village-az"),
-    ).toEqual("https://parking.org#parking-reform-map=saint-shoup-village-az");
+    ).toEqual("https://parking.org#city=saint-shoup-village-az");
   });
 
   test("replaces any existing # in the URL", () => {
     // We may want to make this more intelligent to preserve existing hashes. But we currently
     // don't have any use for hashes other than pre-defining the city. So this is simpler.
     expect(
-      determineShareUrl("https://parking.org#already-hash", "city-az"),
-    ).toEqual("https://parking.org#parking-reform-map=city-az");
+      determineShareUrl("https://parking.org#already-hash", "tempe-az"),
+    ).toEqual("https://parking.org#city=tempe-az");
     expect(
-      determineShareUrl(
-        "https://parking.org#parking-reform-map=another-city-ny",
-        "city-az",
-      ),
-    ).toEqual("https://parking.org#parking-reform-map=city-az");
+      determineShareUrl("https://parking.org#city=another-city-ny", "tempe-az"),
+    ).toEqual("https://parking.org#city=tempe-az");
   });
 });
